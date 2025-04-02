c5eb8af914d43a02160ef85c4f3a30fbff5913e1
mentor <davidnwachukwumgmail.com>
2025-04-02 10:03:46 +0100

    testing and fixing bugs

--- src/controllers/house-controller.js
+++ src/controllers/house-controller.js
@@ -30,7 +30,7 @@ async function get_house (req, res) {
     
     try{
       
-            const data = await house_service.find_house(type, keyword,min,max,category,location,limit,bardge)
+            const data = await house_service.find_house(type, keyword,parseInt(min),parseInt(max),category,location,parseInt(limit),toInteger(bardge))
             const responseData = response.goodResponse
             responseData.data = data
             return res.json(responseData).status(200)
