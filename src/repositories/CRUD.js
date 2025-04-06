630b4676067e8d3a560d7130fa49cf4405ed11bd
mentor <davidnwachukwumgmail.com>
2025-04-06 23:13:21 +0100

    testing ad debuging

--- src/repositories/CRUD.js
+++ src/repositories/CRUD.js
@@ -122,7 +122,7 @@ class crudRepositoryExtra {
     if (filter.category && filter.category !== "undefined")
       query.category = new RegExp(filter.category, "i");
     if(query.id)
-      query.id = {$ne:query.id};
+      query.id = {$ne:{_id:query.id}};
     // Price filter
     const min = Number(filter.min);
     const max = Number(filter.max);
